e4b0e4d6c8c3463e8f6c8814de869e43fc3f8815
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-24 20:37:48 +0800

    Update main.js

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -65,6 +65,6 @@ angular.module('todoController', [])
 					$scope.loading = false;
 					$scope.formData = {}; // clear the form so our user is ready to enter another
 					$scope.todos = data; // assign our new list of todos
-				})
+				});
 		};
 	}]);
