aafb4cfc9601bf55abd93cb2a595ca871ba4c785
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-25 20:14:25 +0800

    Update routes.js

--- files/mean-docker/express-server/app/routes.js
+++ files/mean-docker/express-server/app/routes.js
@@ -42,8 +42,7 @@ module.exports = function (app) {
     // delete a todo
     app.delete('/api/todos/:todo_id', function (req, res) {
         Todo.remove({
-            //_id: req.params.todo_id
-            username: 'abc'
+            _id: req.params.todo_id
         }, function (err, todo) {
             if (err)
                 res.send(err);
@@ -53,7 +52,7 @@ module.exports = function (app) {
     });
 
     //更新一个todo的存款
-    app.put('/api/todos:todo_id', function(req,res){
+    /*app.put('/api/todos:todo_id', function(req,res){
         Todo.update({
             //_id: req.params.todo_id
             username: 'abc'
@@ -67,7 +66,7 @@ module.exports = function (app) {
 
             getTodos(res);
         });
-    });
+    });*/
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
         res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
