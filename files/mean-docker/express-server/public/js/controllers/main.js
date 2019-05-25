53930b51b7d780bbba86ce60ee7046fe4633ba5b
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-25 20:13:32 +0800

    Update main.js

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -3,6 +3,7 @@ angular.module('todoController', [])
 	// inject the Todo service factory into our controller
 	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
 		$scope.formData = {};
+		$scope.updateData = {};
 		$scope.loading = true;
 	
 		// GET =====================================================================
