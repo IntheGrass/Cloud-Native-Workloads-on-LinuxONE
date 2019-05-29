12ee67ad70964d44cf1e7787f0dad266a968f52c
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-29 17:28:10 +0800

    Update routes.js

--- files/mean-docker/express-server/app/routes.js
+++ files/mean-docker/express-server/app/routes.js
@@ -31,8 +31,12 @@ module.exports = function (app) {
     // get all todos
     app.get('/api/todos', function (req, res) {
         // use mongoose to get all todos in the database
-        if (req)
-        getTodos(res);
+        if (!req.body){
+            getTodos(res);
+        }
+        else{
+            getLandUser(req, res);
+        }
     });
 
     // create todo and send back all todos after creation
