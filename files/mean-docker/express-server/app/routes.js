5bca5665b509a744be8571fdda200232be0b55b5
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-30 16:33:42 +0800

    Update routes.js

--- files/mean-docker/express-server/app/routes.js
+++ files/mean-docker/express-server/app/routes.js
@@ -77,7 +77,10 @@ module.exports = function (app) {
             getTodos(res);
         });
     });
-
+    
+    app.put('/api/todos', function (req, res) {
+       getLandUser(req,res);
+    });
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
         res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
