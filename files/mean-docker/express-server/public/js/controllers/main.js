2b5f36c6a4900293071e41ab1b492973930df3ad
IntheGrass <40078140+IntheGrass@users.noreply.github.com>
2019-05-31 11:07:10 +0800

    Update main.js

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -100,17 +100,21 @@ angular.module('todoController', [])
 			console.log($scope.updateData.balance);
 			Todos.update($scope.todos[fromID]._id,$scope.updateData)
 				.success(function(data){
-				$scope.todos = data;
-			});
-			$scope.updateData.balance = $scope.todos[$scope.transferData.id].balance + $scope.transferData.balance;//计算增加后的余额
-			//更新被转账者的数据
-			Todos.update($scope.todos[$scope.transferData.id]._id,$scope.updateData)
-				.success(function(data){
 				$scope.loading = false;
 				$scope.transferData = {};
 				$scope.todos = data;
 				$scope.showData = $scope.todos[fromID];
 			});
+			$scope.updateData.balance = $scope.todos[$scope.transferData.id].balance + $scope.transferData.balance;//计算增加后的余额
+			console.log($scope.updateData.balance);
+			//更新被转账者的数据
+			/*Todos.update($scope.todos[$scope.transferData.id]._id,$scope.updateData)
+				.success(function(data){
+				$scope.loading = false;
+				$scope.transferData = {};
+				$scope.todos = data;
+				$scope.showData = $scope.todos[fromID];
+			});*/
 		};
 
 	}]);
